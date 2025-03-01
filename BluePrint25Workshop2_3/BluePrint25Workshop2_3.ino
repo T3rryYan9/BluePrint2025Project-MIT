2cefddba735edb8aecdbeca379013dba6ec8d558
Shuwhits <123417941+T3rryYan9@users.noreply.github.com>
2025-03-01 17:09:20 -0500

    Update BluePrint25Workshop2_3.ino

--- BluePrint25Workshop2_3/BluePrint25Workshop2_3.ino
+++ BluePrint25Workshop2_3/BluePrint25Workshop2_3.ino
@@ -2,15 +2,21 @@
 #include <Wire.h>
 #include <Adafruit_SSD1306.h>
 #include <Adafruit_GFX.h>
+#include <Servo.h>
 
 #define LED 11
 #define BUTTON 12
+#define SERVO_PIN 6
 #define POT_PIN A0 //Potentiometer
 #define ULTRASONIC_TRIGGER 9
 #define ULTRASONIC_ECHO 10
+#define BUZZER 3
 
 int max_ultrasonic_distance_cm = 400; //sets max distance on how far it will detect
 NewPing sonar(ULTRASONIC_TRIGGER, ULTRASONIC_ECHO, max_ultrasonic_distance_cm); //intialization of sensor
+
+Servo servo;
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -21,14 +27,26 @@ void setup() {
   pinMode(LED, OUTPUT);
   pinMode(BUTTON, INPUT_PULLUP);
   pinMode(POT_PIN, INPUT); 
+  pinMode(BUZZER, OUTPUT);
 
-
+  servo.attach(SERVO_PIN);
+  servo.write(0);
+  
+  digitalWrite(BUZZER, HIGH);
+  delay(1000);
+  digitalWrite(BUZZER, LOW);
 }
 
 void read_ultrasonic_sensor(){
   int max_ultrasonic_distance_cm = sonar.ping_cm();
   Serial.print("Ultrasonic: ");
   Serial.print(max_ultrasonic_distance_cm);
+}
+
+void buzz() {
+  digitalWrite(BUZZER, HIGH);
+  delay(1);
+  digitalWrite(BUZZER, LOW);
 }
 
 void loop() {
@@ -38,5 +56,10 @@ void loop() {
   Serial.println(pot_value);// Constantly prints out the value of the potentiometer
   int brightness = map(pot_value, 0, 1023, 0, 255); //Maps out potentiometer values
   analogWrite(LED, brightness); // writes the brightness of the led/""
-  read_ultrasonic_sensor();
+  // read_ultrasonic_sensor();
+
+  int angle = map(pot_value, 0, 1023, 0, 180); 
+  // Serial.println(a)
+  servo.write(angle);
+
 }
