3c49a1c686d49fcb835872e4b9b5598d91a4822a
ur mom <condruposeidon@gmail.com>
2025-03-02 09:23:47 -0500

    Add first test

--- BlueprintPong/BlueprintPong/BlueprintPong.ino
+++ BlueprintPong/BlueprintPong/BlueprintPong.ino
@@ -1,9 +1,33 @@
-void setup() {
-  // put your setup code here, to run once:
+#define LED_5 22
+#define LED_6 24
+#define LED_7 26
 
+void setup() {
+  pinMode(LED_5, OUTPUT);
+  pinMode(LED_6, OUTPUT);
+  pinMode(LED_7, OUTPUT);
+  Serial.begin(9600);
+  Serial.println("Led Starting...");
+
+}
+
+void blinkLED() {
+  digitalWrite(LED_5,HIGH);
+  delay(300);
+  digitalWrite(LED_5,LOW);
+  delay(300);
+  digitalWrite(LED_6,HIGH);
+  delay(300);
+  digitalWrite(LED_6,LOW);
+  delay(300);
+  digitalWrite(LED_7,HIGH);
+  delay(300);
+  digitalWrite(LED_7,LOW);
+  delay(300);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  blinkLED();
 
 }
