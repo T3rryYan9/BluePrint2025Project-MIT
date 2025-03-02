45c74c391723417f079c5125c4f50a7b84b623cf
ur mom <condruposeidon@gmail.com>
2025-03-02 09:43:41 -0500

    test 2

--- BlueprintPong/BlueprintPong/BlueprintPong.ino
+++ BlueprintPong/BlueprintPong/BlueprintPong.ino
@@ -1,29 +1,34 @@
-#define LED_5 22
-#define LED_6 24
-#define LED_7 26
+// #define LED_22 22
+// #define LED_24 24
+// #define LED_26 26
+// #define LED_28 28
+// #define LED_30 30
+// #define LED_32 32
+// #define LED_34 34
+// #define LED_36 36
+// #define LED_38 38
+// #define LED_40 40
+// #define LED_42 42
+// #define LED_44 44
+
+const int ledPins[] = {22,24,26,28,30,32,34,36,38,40,42,44};
 
 void setup() {
-  pinMode(LED_5, OUTPUT);
-  pinMode(LED_6, OUTPUT);
-  pinMode(LED_7, OUTPUT);
+  for (int i = 0; i < 12; i++) {
+    pinMode(ledPins[i],OUTPUT);
+  }
   Serial.begin(9600);
   Serial.println("Led Starting...");
 
 }
 
 void blinkLED() {
-  digitalWrite(LED_5,HIGH);
-  delay(300);
-  digitalWrite(LED_5,LOW);
-  delay(300);
-  digitalWrite(LED_6,HIGH);
-  delay(300);
-  digitalWrite(LED_6,LOW);
-  delay(300);
-  digitalWrite(LED_7,HIGH);
-  delay(300);
-  digitalWrite(LED_7,LOW);
-  delay(300);
+  for (int i = 0; i < 12; i++) {
+    digitalWrite(ledPins[i],HIGH);
+    delay(20);
+    digitalWrite(ledPins[i],LOW);
+    delay(20);
+  }
 }
 
 void loop() {
